52438adaecc29fa49bdcee9fe92970aa68b8b971
taiseiiiii <isemiya.0509@gmail.com>
2025-09-29 17:58:29 -0700

    complete ex06

--- ex06.js
+++ ex06.js
@@ -2,5 +2,13 @@
 // an object containing the query parameters as key-value pairs.
 // Example: getQueryParams("https://example.com?search=test&page=2")
 // should return { search: "test", page: "2" }.
+const getQueryParams = (url) => {
+  const queryString = url.slice(url.indexOf("?") + 1);
+  return queryString.split("&").reduce((res, e) => {
+    const [key, value] = e.split("=");
+    res[key] = value;
+    return res;
+  }, {});
+};
 
 console.log(getQueryParams("https://example.com?search=test&page=2")); // Expected output: { search: "test", page: "2" }
