e13cd6aa13ca039c31e3973b28dba134e6aee171
taiseiiiii <isemiya.0509@gmail.com>
2025-09-29 18:09:24 -0700

    complete ex09

--- ex09.js
+++ ex09.js
@@ -3,4 +3,16 @@
 // Example: sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")
 // should return [{name: "Bob", age: 25}, {name: "Alice", age: 30}].
 
-console.log(sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")); // Expected output: [{name: "Bob", age: 25}, {name: "Alice", age: 30}]
+const sortByProperty = (arr, target) => {
+  return arr.sort((a, b) => a[target] - b[target]);
+};
+
+console.log(
+  sortByProperty(
+    [
+      { name: "Alice", age: 30 },
+      { name: "Bob", age: 25 },
+    ],
+    "age"
+  )
+); // Expected output: [{name: "Bob", age: 25}, {name: "Alice", age: 30}]
