08b3449de914dee830c3f43e7a08aa19bbbb37cb
taiseiiiii <isemiya.0509@gmail.com>
2025-09-29 18:02:35 -0700

    complete ex07

--- ex07.js
+++ ex07.js
@@ -3,5 +3,12 @@
 // (e.g., the presence of "@" and a ".").
 // Example: validateEmail("test@example.com") should return true.
 
+const validateEmail = (email) => {
+  const atIndex = email.indexOf("@");
+  const dotIndex = email.lastIndexOf(".");
+
+  return atIndex > 0 && dotIndex > atIndex + 1 && dotIndex < email.length - 1;
+};
+
 console.log(validateEmail("test@example.com")); // Expected output: true
 console.log(validateEmail("invalid-email.com")); // Expected output: false
